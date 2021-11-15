e1e8deffa5eeee0db5d841732cafd47c376bb127
Pedro Barbosa <pedro_dith@hotmail.com>
2021-11-14 21:14:40 -0300

    att dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
+import React from 'react';
 import RouteComponent from './route';
 
 function App() {
    return (
-         <div className="App">
-            <RouteComponent />
-         </div>
+      <div className="App">
+         <RouteComponent />
+      </div>
    );
 }
 
